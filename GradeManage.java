f493613e0d92f87e7725fb0922e27f1b7e2588bd
이상민 <tkdals020605@gmail.com>
2026-05-11 23:52:47 +0900

    MyApp 작성 및 GradeManage get 메소드 추가

--- GradeManage.java
+++ GradeManage.java
@@ -14,17 +14,35 @@ public class GradeManage
     private int studentCount;
     private int subjectCount;
 
+
+    Scanner sc = new Scanner(System.in);
+
     /**
      * GrandManage 클래스의 객체 생성자
      */
     public GradeManage()
     {
         // 인스턴스 변수 초기화
+
         this.students = new Student[100];
         this.subjects = new Subject[100];
         this.grades = new Grade[100][100];
         this.studentCount = 0;
         this.subjectCount = 0;
+        this.studentCount = 0;
+        this.subjectCount = 0;
+    }
+
+    public int getStudentCount()
+    {
+        // 여기에 코드를 작성하세요.
+        return studentCount;
+    }
+    
+    public int getSubjectCount()
+    {
+        // 여기에 코드를 작성하세요.
+        return subjectCount;
     }
     
     public Grade[][] getGrades()
@@ -45,28 +63,46 @@ public class GradeManage
         return subjects;
     }
     
-    public void registerStudent(Scanner sc)
+    public void addStudent(Student student)
     {
         // 여기에 코드를 작성하세요.
-        Student student = Student.registerStudent(sc);
-        for(int i = 0; i < students.length; i++){
-            students[i] = student;
-        }
-    }
-    
-    public void registerSubject(Scanner sc)
-    {
-        // 여기에 코드를 작성하세요.
-        Subject subject = Subject.registerSubject(sc);
-        for(int i = 0; i < subjects.length; i++){
-            subjects[i] = subject;
-        }
-    }
-    
-    public void registerGrade(Scanner sc)
-    {
-        // 여기에 코드를 작성하세요.
-        Grade grade = Grade.registerGrade(sc);
+        students[studentCount] = student;
+        studentCount++;
     }
 
+    public void addSubject(Subject subject)
+    {
+        // 여기에 코드를 작성하세요.
+        subjects[subjectCount] = subject;
+        subjectCount++;
+    }
+
+    public void inputGrade(Scanner sc)
+    {
+        // 여기에 코드를 작성하세요.
+
+        System.out.print("학생 학번 입력 : ");
+        int studId = sc.nextInt();
+        int studentIndex = -1;
+        
+        for(int i = 0; i < studentCount; i++){
+            if(students[i].getStudId().equals(studId)){
+                studentIndex = i;
+            }
+        }
+        
+        System.out.print("과목 코드 입력 : ");
+        int subjectCode = sc.nextInt() - 1;
+        int subjectIndex = -1;
+        
+        for(int i = 0; i < subjectCount; i++){
+            if(subjects[i].getSubjectCode().equals(subjectCode)){
+                subjectIndex = i;
+            }
+        }
+        
+        Grade grade = Grade.registerGrade(sc, students[studentIndex], subjects[subjectIndex]);
+        grades[studentIndex][subjectIndex] = grade;
+        System.out.println("성적 등록 완료");
+    }
 }
--- MyApp.java
+++ MyApp.java
@@ -2,16 +2,64 @@ import java.util.Scanner;
 /**
  * MyApp 클래스의 설명을 작성하세요.
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @author (2022320029_이상민)
+ * @version (2026.05.11)
  */
 public class MyApp
 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        GradeManage manage = new GradeManage();
         
-        GrandManage manage = new GrandManage();
+        int choice = -1;
         
-        manage
+        do {
+            System.out.println("1. 학생 등록");
+            System.out.println("2. 과목 등록");
+            System.out.println("3. 성적 입력");
+            System.out.println("4. 성적 출력");
+            System.out.println("0. 종료");
+            
+            try{
+                choice = Integer.parseInt(sc.nextLine().trim());
+            }
+            catch (NumberFormatException e){
+                System.out.println("숫자를 입력해주세요.");
+            }
+            
+            switch (choice){
+                case 1:
+                    manage.registerStudent(sc);
+                    break;
+                case 2:
+                    manage.registerSubject(sc);
+                    break;
+                case 3:
+                    manage.registerGrade(sc);
+                    break;
+                case 4:
+                    GradeCalculator.calculateRelativeAllGrades(
+                    manage.getAllGrades(),
+                    manage.getSubject(),
+                    manage.getStudentCount(),
+                    manage.getSubjectCount()
+                    );
+                    Print.printAllGrades(
+                    manage.getAllGrades(),
+                    manage.getStudents(),
+                    manage.getSubject(),
+                    manage.getStudentCount(),
+                    manage.getSubjectCount()
+                    );
+                    break;
+                case 0:
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                defalut:
+                    System.out.println("올바른 번호를 입력해주세요.");
+            }
+        }
+        while (choice != 0);
+        sc.close();
     }
 }
